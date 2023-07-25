de5aec7fcd34a16b6443298da8a75046e9843dda
loudringphone <loudringphone@gmail.com>
2023-07-25 22:30:10 +1000

    learning how to upload files to S3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "groceryhelper": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
   "auth": {
     "groceryhelper4dd16055": {
       "AppClientID": "string",
@@ -15,12 +8,6 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
-    }
-  },
-  "storage": {
-    "InvoiceStorage": {
-      "BucketName": "string",
-      "Region": "string"
     }
   }
 }
